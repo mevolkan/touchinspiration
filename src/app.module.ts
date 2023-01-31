5878750ad00f72d4bbcbd0817ce870e3c5fde59b
volkan <16579273+mevolkan@users.noreply.github.com>
2023-01-31 10:55:48 +0300

    typorm import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WalletModule } from './wallet/wallet.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, WalletModule, TransactionsModule],
+  imports: [
+    UsersModule,
+    WalletModule,
+    TransactionsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'postgres',
+      password: 'root',
+      database: 'touch',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
