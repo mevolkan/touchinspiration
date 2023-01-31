ac74c799fdb1f57331826a23f3886e03255393d7
volkan <16579273+mevolkan@users.noreply.github.com>
2023-01-31 10:17:54 +0300

    transactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WalletModule } from './wallet/wallet.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [UsersModule, WalletModule],
+  imports: [UsersModule, WalletModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
